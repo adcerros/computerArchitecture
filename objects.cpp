d86261e9eea25f586173afdd130941660c0e071b
Adrian Cerros <adriancerrossanchez@gmail.com>
2021-10-09 18:25:11 +0200

    commit inicial

---
+++ objects.cpp
@@ -0,0 +1,19 @@
+
+
+// Variables globales
+int time = 0;
+
+//Definicion del objeto
+struct object {
+        parameters = []
+        };
+
+struct recinto {
+
+}
+
+
+int main (int num_objects, int num_interations, int random_seed, float size_enclosure ){
+
+}
+
